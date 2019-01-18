da8e51643aef4efd0664d196ed77b48e9eff7d4c
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-18 23:13:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -81,8 +81,8 @@ def organize_schools(schools)
     by_location[location.values[0]] = []
   end
   schools.each do |school, location|
-    if by_location[location.value[0]] == location
-      by_loacation[location.value[0]] << school
+    if by_location[location.values[0]] == location
+      by_loacation[location.values[0]] << school
     # by_location[location.values[0]] = school
   end
 end
