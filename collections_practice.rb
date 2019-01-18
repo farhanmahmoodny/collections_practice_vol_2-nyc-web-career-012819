735d624288d6865884fdf66f9eb72a12b59f004a
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-18 22:45:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -84,7 +84,7 @@ def organize_schools(hash
   locations = {}
   hash.values.each { |location| locations[location.values[0]] = [] }
   hash.each { |school, location| locations[location.values[0]] << school }
-  end
+
 end
 locations
 end
